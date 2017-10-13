67474d95e38c018741c0132bbf0ff6d8cfc0a830
Josh Nothum <joshnothum@gmail.com>
2017-10-13 17:01:20 -0500

    client is successfully post information to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,33 @@ $(document).ready(weReady);
 
 function weReady() {
     console.log('js sourced');
+    clickHandler();
 }
+
+function clickHandler() {
+    $('#addition').on('click', addItUp);
+    
+}
+function addItUp() {
+     event.preventDefault();
+     firstNumber = $('#firstNumber').val();
+     secondNumber = $('#secondNumber').val();
+     var operator = 'add';
+     console.log(firstNumber, secondNumber, operator);
+     var addData = {firstNumber, secondNumber, operator};
+     console.log(addData);
+
+     $.ajax({
+         method:"POST",
+         url: '/addition',
+         data: addData
+     }).done(function(response){
+        console.log('success');
+         
+     }).fail(function(message){
+         console.log('fail');
+         
+     
+     });
+     
+ }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,28 @@ var port = 5000;
 
 app.use(express.static('server/public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var result = [];
+
+app.post('/addition', function(req, res){
+    console.log(req.body);
+    console.log(req.body.firstNumber);
+    var firstNumber = req.body.firstNumber;
+    var secondNumber = req.body.secondNumber;
+    var operator = req.body.operator;
+
+    function addItUpServer(){
+        newNumber = (firstNumber + secondNumber);
+        result.push(newNumber);
+    }
+    
+    
+    res.sendStatus(201);
+    
+});
+
 
 app.listen(port, function () {
     console.log('listening on port:', port);
