c0fc8d009536d34f83b7c542715bee7cf2a77d30
Josh Nothum <joshnothum@gmail.com>
2017-10-20 13:35:14 -0500

    port change

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 var calcRouter = require('./routes/newCalculator_router.js');
 
 app.use(express.static('server/public'));
